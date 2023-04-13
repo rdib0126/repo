28d68dd9777e4311635287ecc5680c9c45f5b5b7
SAN.D <31897843+SANKARAMDAS@users.noreply.github.com>
2023-04-13 14:52:55 +0530

    Create console.js

---
+++ console.js
@@ -0,0 +1,8 @@
+200 - Everything went well
+404 - page not found (request not found)
+
+100 - informational response
+200 - Successful
+300 - Redirections
+400 - Client-side errors
+500 - Server-side errors
