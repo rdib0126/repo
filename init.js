b9edb9dbbcce2dd83e0b438f74f35c0893dcde16
SAN.D <31897843+SANKARAMDAS@users.noreply.github.com>
2023-04-13 15:40:42 +0530

    CRUD

---
+++ init.js
@@ -0,0 +1,14 @@
+CRUD - Create(POST) Read(GET) Update(PUT) Delete(DELETE)
+
+4 basic Http verbs - GET POST PUT DELETE
+
+status code -
+
+200 - Everything went well
+404 - page not found (request not found)
+
+100 - informational response
+200 - Successful
+300 - Redirections
+400 - Client-side errors
+500 - Server-side errors
